a84bd4d0b5905bd401f5e6cfe5085fb4b00616f2
victor-hugo-amaral <189042440+victor-hugo-amaral@users.noreply.github.com>
2025-03-26 20:12:58 -0300

    while

--- aula12ex/ex015/ex015.js
+++ aula12ex/ex015/ex015.js
@@ -15,14 +15,37 @@ function verificar() {
         let fsex = document.getElementsByName('radsex')
         let idade = ano - Number(anoInput.value)
         let genero = ''
+        let img = document.createElement('img')
         // checked == Marcado ou checado
         if (fsex[0].checked) {
             genero = 'Homem'
+            if (idade >= 1 && idade < 10) {
+                img.setAttribute('src', 'img/menino.png')
+            } else if (idade < 21) {
+                img.setAttribute('src', 'img/jovenh.png')
+            } else if (idade < 50) {
+                img.setAttribute('src', 'img/homem.png')
+            } else {
+                img.setAttribute('src', 'img/velho.png')
+            }
+
         } else if (fsex[1].checked) {
             genero = 'Mulher'
+            if (idade >= 1 && idade < 10) {
+                img.setAttribute('src', 'img/menina.png')
+            } else if (idade < 21) {
+                img.setAttribute('src', 'img/jovenm.png')
+            } else if (idade < 50) {
+                img.setAttribute('src', 'img/mulher.png')
+            } else {
+                img.setAttribute('src', 'img/velha.png')
+            }
         } else {
             alert('Escolha um genero')
         }
-        res.style.textAlign = "center"
+        //appendChild = Adiciona o elemento dentro dos corchetes
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.appendChild(img)
     }
+
 }
