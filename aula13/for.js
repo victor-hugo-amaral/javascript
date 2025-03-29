7aa067985d11316e1bcea461780f1feb52316e26
victor-hugo-amaral <189042440+victor-hugo-amaral@users.noreply.github.com>
2025-03-29 20:11:27 -0300

    aula 16

---
+++ aula13/for.js
@@ -0,0 +1,7 @@
+
+// for (inicio;teste logico;incremento){}
+console.log('Vai comecar')
+for (let c = 1; c <= 5; c++) {
+    console.log(`Passo ${c} `)
+}
+console.log('FIM ')
--- aula13/while.js
+++ aula13/while.js
@@ -1,10 +1,10 @@
-/*
-let c = 1
-while (c <= 10) {
-    console.log(`Passo ${c}`)
-    c++ // c =  c + 1
-}
-*/
+// let c = 1
+// while (c <= 500) {
+//     console.log(`${c} Tudo bem? `)
+//     c++
+// }
+
+
 
 
 /*
@@ -18,20 +18,19 @@ while (n <= 100) {
 }
 console.log(soma)
 */
-
+/*
 function fibo(n) {
     let fib = [0, 1]
     let i = 2
     while (i <= n) {
         fib[i] = fib[i - 1] + fib[i - 2]
         i++
-    }
-    //   O slice pega os elementos da posição 0 até 5(o índice 6 é excluído).
+    }    //   O slice pega os elementos da posição 0 até 5(o índice 6 é excluído).
     //     slice = O índice fim não é incluído, é como se fosse o "final da linha" e tudo antes dele é copiado.
     return fib.slice(0, n)
 }
 console.log(fibo(6))
-
+*/
 
 // let n1 = 1
 // while (n1 <= 10) {
@@ -45,3 +44,14 @@ console.log(fibo(6))
 //     n1--
 // }
 // console.log('Fim da contagem FDP!')
+
+
+/*
+let c = 1
+while (c <= 10) {
+    console.log(`${c}`)ff
+    c++
+}
+*/
+
+
