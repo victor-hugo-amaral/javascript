0152f21fba4f1b6f1d8564216c49aa85369e7a9d
victor-hugo-amaral <189042440+victor-hugo-amaral@users.noreply.github.com>
2025-04-10 10:24:23 -0300

    aula14

--- aula14ex/ex016/ex016.js
+++ aula14ex/ex016/ex016.js
@@ -1,4 +1,41 @@
 const botao = document.getElementById('verificar')
+botao.addEventListener('click', contador)
+function contador() {
+    let inputinicio = document.getElementById('inicio')
+    let inputfim = document.getElementById('fim')
+    let inputpasso = document.getElementById('passo')
+    let res = document.getElementById('res')
+
+    if (inputinicio.value === '' || inputfim.value === '' || inputpasso.value === '') {
+        alert('[ERRO]Digite um numero valido.')
+        return
+    }
+    res.innerHTML = '<p>Contando...</p>'
+    let ini = Number(inputinicio.value)
+    let fim = Number(inputfim.value)
+    let p = Number(inputpasso.value)
+
+    if (p <= 0) {
+        alert('[ERRO]Passos nao preenchido vou consideral como = 1.')
+        p = 1
+    }
+
+
+    if (ini < fim) {
+        for (let c = ini; c <= fim; c += p) {
+            res.innerHTML += `${c} \u{1F449}`
+        }
+
+    }
+    if (ini > fim) {
+        for (let c = ini; c >= fim; c -= p) {
+            res.innerHTML += `${c} \u{1F449}`
+        }
+    }
+    res.innerHTML += `\u{1F3F4}`
+}
+/*
+const botao = document.getElementById('verificar')
 botao.addEventListener('click', verificar)
 function verificar() {
     let inputInicio = document.getElementById('inicio')
@@ -30,3 +67,4 @@ function verificar() {
     }
     res.innerHTML += `\u{1F3F4}`
 }
+*/ 
